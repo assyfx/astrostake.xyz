f7eb1d1c7af7724c54c3012249f86fe36cf49c6a
AstroStake <assyfamusthofa@gmail.com>
2025-05-20 02:53:39 +0700

    add new validator

--- 0g-status/card.js
+++ 0g-status/card.js
@@ -15,13 +15,13 @@ window.RPCS = [
   { name: "zskw", url: "https://0g.galileo.zskw.xyz/", active: true, official: false },
   { name: "shachopra", url: "https://0g-galileo.shachopra.com/", active: true, official: false },
   { name: "CoreNode Community 2", url: "https://0g-galileo-evmrpc2.corenodehq.xyz/", active: true, official: false },
-  { name: "Zeycanode", url: "https://0g-galileo-evm-rpc.zeycanode.com/", active: true, official: false },
   { name: "wukongz", url: "http://wukongz.duckdns.org:8545/", active: true, official: false },
   { name: "skyxyz", url: "https://0g-evm.skyxyz.com/", active: true, official: false },
   { name: "Validator247", url: "http://0g-galileo-evm-rpc.validator247.com/", active: true, official: false },
   { name: "VjnNodes", url: "https://evmrpc.vinnodes.com/", active: true, official: false },
   { name: "ferdimanaa", url: "https://0g-galileo-ferdimanaa.xyz/", active: true, official: false },
-  { name: "xAthzid", url: "https://0g-galileo.xzid.xyz/", active: true, official: false }
+  { name: "xAthzid", url: "https://0g-galileo.xzid.xyz/", active: true, official: false },
+  { name: "Zeycan", url: "https://0g-evm-rpc.zeycanode.com/", active: true, official: false }
 ];
 
 function formatNumber(num) {
